28eaa1c9ece40e5feb3cdb968c26171a852284bd
Marcos <marcosrio.26@gmail.com>
2025-11-02 21:59:18 -0300

    incluida logica para varibles de entorno

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: "localhost",
-  port: 3307,
-  user: "posta-db-username",
-  password: "posta-db-password",
-  database: "posta-db",
+  host: process.env.DB_HOST || "localhost",
+  port: process.env.DB_PORT || 3307,
+  user: process.env.DB_USER || "posta-db-username",
+  password: process.env.DB_PASSWORD || "posta-db-password",
+  database: process.env.DB_NAME || "posta-db",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
